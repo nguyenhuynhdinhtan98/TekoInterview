7055877ace9347eeee7ca63d00efff0721680ee3
Tân Nguyễn <nguyenhuynhdinhtan98@gmail.com>
2023-01-05 12:59:12 +0700

    Initial Commit

---
+++ NGUYENHUYNHDINHTAN_iOS/NGUYENHUYNHDINHTAN_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NGUYENHUYNHDINHTAN_iOSApp.swift
+//  NGUYENHUYNHDINHTAN_iOS
+//
+//  Created by Tân Nguyễn on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NGUYENHUYNHDINHTAN_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
